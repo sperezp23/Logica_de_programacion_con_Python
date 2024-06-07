e24e5ac9f6d0e6246321ad9d8b43e03794af2123
Santiago Pérez Puerta <78920906+sperezp23@users.noreply.github.com>
2024-06-07 00:06:34 -0500

    Update 16_Elementos_comunes.py

--- U_de_A_Fund_de_Computacion_Taller_3/16_Elementos_comunes.py
+++ U_de_A_Fund_de_Computacion_Taller_3/16_Elementos_comunes.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-def valores_unicos(a, b):
+def valores_comunes(a, b):
     return np.in1d(a, np.intersect1d(a,b))
 
 a = np.array([0,  10,  20,  40,  60])
 b = np.array([0, 40])
 
-print(valores_unicos(a,b))
+print(valores_comunes(a,b))
